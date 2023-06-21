501ef55ddfa214baa99051af58c49709be23f108
Rizwan Ahamad <rizwanahmd14@gmail.com>
2023-06-21 21:19:49 +0530

    Updates

---
+++ precache-manifest.e4acb346f7c55cd8557d1750460bef68.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae2120ad97f5f5954590",
+    "url": "/FoodFast/static/css/main.409d9286.chunk.css"
+  },
+  {
+    "revision": "ae2120ad97f5f5954590",
+    "url": "/FoodFast/static/js/main.ae2120ad.chunk.js"
+  },
+  {
+    "revision": "c1158deaed1b9c59e054",
+    "url": "/FoodFast/static/css/1.ca76a15d.chunk.css"
+  },
+  {
+    "revision": "c1158deaed1b9c59e054",
+    "url": "/FoodFast/static/js/1.c1158dea.chunk.js"
+  },
+  {
+    "revision": "e0ac3eddbf0979928d55",
+    "url": "/FoodFast/static/js/runtime~main.e0ac3edd.js"
+  },
+  {
+    "revision": "729f134cdaf04758c2881bb7d984b8bf",
+    "url": "/FoodFast/static/media/bread(3).729f134c.png"
+  },
+  {
+    "revision": "2b82a8a1f41923e2d0741a2fefbb0f29",
+    "url": "/FoodFast/static/media/res-logo.2b82a8a1.png"
+  },
+  {
+    "revision": "59389592bc3011abf3748bf797d67369",
+    "url": "/FoodFast/static/media/bread(2).59389592.png"
+  },
+  {
+    "revision": "e2b501cd2ca96f64900c75be83c709b6",
+    "url": "/FoodFast/static/media/product_01.e2b501cd.jpg"
+  },
+  {
+    "revision": "5569a8c88a47601cb6f78483522b3e46",
+    "url": "/FoodFast/static/media/product_01.1.5569a8c8.jpg"
+  },
+  {
+    "revision": "7147ade4ba1c0f2b44b71ad779285b85",
+    "url": "/FoodFast/static/media/product_01.3.7147ade4.jpg"
+  },
+  {
+    "revision": "682a4ee916bfcb6aa45e24bbffafc65e",
+    "url": "/FoodFast/static/media/product_2.1.682a4ee9.jpg"
+  },
+  {
+    "revision": "fa40b3f65a2b6450db3d56d412459d0b",
+    "url": "/FoodFast/static/media/product_2.2.fa40b3f6.jpg"
+  },
+  {
+    "revision": "f487629b7b126d215c88e7b54b18a7af",
+    "url": "/FoodFast/static/media/product_2.3.f487629b.jpg"
+  },
+  {
+    "revision": "078c31762872eaa635513f3ca3e6b77d",
+    "url": "/FoodFast/static/media/product_3.1.078c3176.jpg"
+  },
+  {
+    "revision": "f472dbda5e92070bc37434f6d4c70aaa",
+    "url": "/FoodFast/static/media/product_3.2.f472dbda.jpg"
+  },
+  {
+    "revision": "31ef411b43cf35a430d7cf14a9e9e74f",
+    "url": "/FoodFast/static/media/product_3.3.31ef411b.jpg"
+  },
+  {
+    "revision": "a841dccff0f9e05b445928bac1e38f71",
+    "url": "/FoodFast/static/media/product_4.1.a841dccf.jpg"
+  },
+  {
+    "revision": "8f4ab131e63f718f78422d4a66bee6ef",
+    "url": "/FoodFast/static/media/product_4.2.8f4ab131.jpg"
+  },
+  {
+    "revision": "0faf4cfd194d01528e7d8398d3e3219d",
+    "url": "/FoodFast/static/media/product_4.3.0faf4cfd.png"
+  },
+  {
+    "revision": "dba982c08133ad30e9217159b686ba06",
+    "url": "/FoodFast/static/media/product_04.dba982c0.jpg"
+  },
+  {
+    "revision": "9bea83b4e4d32584920f4ac4e65a2ac1",
+    "url": "/FoodFast/static/media/product_08.9bea83b4.jpg"
+  },
+  {
+    "revision": "38b58292d9f6b79b4f6de61d6d59137a",
+    "url": "/FoodFast/static/media/product_09.38b58292.jpg"
+  },
+  {
+    "revision": "26a4a2136a2d62229da878921cb353e3",
+    "url": "/FoodFast/static/media/bread(1).26a4a213.png"
+  },
+  {
+    "revision": "4592a09b3bab3c19fc2e7bc383581004",
+    "url": "/FoodFast/static/media/hero.4592a09b.png"
+  },
+  {
+    "revision": "853f5324e100dadc4167a6bd0598ee50",
+    "url": "/FoodFast/static/media/location.853f5324.png"
+  },
+  {
+    "revision": "d89a48546bf6ab4ca6e9fe036a22e7ee",
+    "url": "/FoodFast/static/media/network.d89a4854.png"
+  },
+  {
+    "revision": "0b074da0ba444449585f1516f6f74587",
+    "url": "/FoodFast/static/media/ava-1.0b074da0.jpg"
+  },
+  {
+    "revision": "32a3b1b08f15e0306b83317904de0d54",
+    "url": "/FoodFast/static/media/ava-2.32a3b1b0.jpg"
+  },
+  {
+    "revision": "10c69124027190f1a753dbb7de4b9da4",
+    "url": "/FoodFast/static/media/ava-3.10c69124.jpg"
+  },
+  {
+    "revision": "03a8d20976540827a226a65872becdd9",
+    "url": "/FoodFast/static/media/banner-02.03a8d209.jpg"
+  },
+  {
+    "revision": "881fbc46361e0c0e5f003c159b2f3005",
+    "url": "/FoodFast/static/media/remixicon.881fbc46.woff"
+  },
+  {
+    "revision": "9915fef980fa539085da55b84dfde760",
+    "url": "/FoodFast/static/media/remixicon.9915fef9.woff2"
+  },
+  {
+    "revision": "31d28485e1cf7369272270fd730327c0",
+    "url": "/FoodFast/static/media/remixicon.31d28485.eot"
+  },
+  {
+    "revision": "888e61f04316f10bddfff7bee10c6dd0",
+    "url": "/FoodFast/static/media/remixicon.888e61f0.ttf"
+  },
+  {
+    "revision": "95138f36e015ad912c37db92164f5844",
+    "url": "/FoodFast/static/media/remixicon.95138f36.svg"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/FoodFast/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/FoodFast/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/FoodFast/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/FoodFast/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "6bdaa6b72d50b7077cd3e985c89a0c12",
+    "url": "/FoodFast/index.html"
+  }
+];
